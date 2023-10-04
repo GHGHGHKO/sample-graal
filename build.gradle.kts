4f370bcc4883e775a94b5ce1a3a30c35e5c3eb26
GHGHGHKO <gudrb963@gmail.com>
2023-10-04 19:48:56 +0900

    fix: `InvalidPluginException` error

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+	id("java")
 	id("org.springframework.boot") version "3.1.4"
 	id("io.spring.dependency-management") version "1.1.3"
 	id("org.hibernate.orm") version "6.2.9.Final"
